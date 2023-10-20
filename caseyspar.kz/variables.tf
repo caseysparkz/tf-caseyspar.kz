7bee539206a3f343ff8dc7d5579b1d26df482154
caseysparkz <himself@caseyspar.kz>
2023-10-20 13:36:36 -0700

    Formatting change to variables.tf.

--- caseyspar.kz/variables.tf
+++ caseyspar.kz/variables.tf
@@ -5,9 +5,9 @@
 ## AWS ========================================================================
 variable "aws_region" {
   description = "AWS region to deploy to."
-  default     = "us-west-2"
   type        = string
   sensitive   = false
+  default     = "us-west-2"
 
   validation {
     condition     = contains(["us-east-1", "us-west-2"], var.aws_region)
@@ -29,12 +29,12 @@ variable "aws_secret_key" {
 
 variable "ecr_repository_names" {
   description = "List of ECR repository names to create."
+  type        = list(string)
+  sensitive   = false
   default = [
     "alpine_base",
     "python3_base"
   ]
-  type      = list(string)
-  sensitive = false
 
   validation {
     condition = alltrue([
@@ -53,59 +53,59 @@ variable "cloudflare_api_token" {
 
 variable "mx_servers" {
   description = "MX servers for root domain. Syntax: {server: priority}."
+  type        = map(string)
+  sensitive   = false
   default = {
     "mail.protonmail.ch"    = 10
     "mailsec.protonmail.ch" = 20
   }
-  type      = map(string)
-  sensitive = false
 }
 
 variable "dkim_records" {
   description = "DKIM (CNAME) for root domain. Syntax: {host: pointer}."
+  type        = map(string)
+  sensitive   = false
   default = {
     "protonmail._domainkey"  = "protonmail.domainkey.dlnhnljjvlx6bfjjsleqnfvszipy5ver4qdnit24cx6ufem2qzgfq.domains.proton.ch"
     "protonmail2._domainkey" = "protonmail2.domainkey.dlnhnljjvlx6bfjjsleqnfvszipy5ver4qdnit24cx6ufem2qzgfq.domains.proton.ch"
     "protonmail3._domainkey" = "protonmail3.domainkey.dlnhnljjvlx6bfjjsleqnfvszipy5ver4qdnit24cx6ufem2qzgfq.domains.proton.ch"
   }
-  type      = map(string)
-  sensitive = false
 }
 
 variable "spf_senders" {
   description = "List of allowed SPF senders, like: [\"include:_spf.example.com\", \"ip4:127.0.0.1\"]."
+  type        = list(string)
   default = [
     "include:_spf.protonmail.ch",
     "mx"
   ]
-  type      = list(string)
   sensitive = false
 }
 
 variable "verification_records" {
   description = "List of verification TXT records for root domain."
+  type        = list(string)
+  sensitive   = false
   default = [
     "keybase-site-verification=8MXkrublC6Bg6NPBvHAwmK12v1FledQETZS1ux_oi0A", #  Keybase.
     "protonmail-verification=fe3be76ae32c8b2a12ec3e6348d6a598e4e4a4f3"       #  Proton.
   ]
-  type      = list(string)
-  sensitive = false
 }
 
 variable "pka_records" {
   description = "Map of PKA handles and fingerprints for root domain."
+  type        = map(string)
   default = {
     himself = "133898B4C51BC39479E97F1B2027DEDFECE6A3D5"
   }
-  type      = map(string)
   sensitive = false
 }
 
 variable "forward_zones" {
   description = "Forward zones for the root domain."
-  default     = ["cspar.kz"]
   type        = list(string)
   sensitive   = false
+  default     = ["cspar.kz"]
 }
 
 ## Misc. ======================================================================
