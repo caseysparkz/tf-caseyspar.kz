fda45d591476674198351ddad8021a1c4fe5f728
caseysparkz <himself@caseyspar.kz>
2024-05-14 13:24:40 -0700

    Moved frontend > frontend/www to allow for frontends on different subdomains. Added README.

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+This repository contians complete configurations for my personal cloud and LAN and infrastructure.
+
+It is always a work in progress.
--- caseyspar.kz/main.tf
+++ caseyspar.kz/main.tf
@@ -131,7 +131,7 @@ module "www" { # -------------------------------------------------------------- 
   api_gateway_root_resource_id = module.api.aws_api_gateway_root_resource_id
   api_gateway_execution_arn    = module.api.aws_api_gateway_execution_arn
   site_title                   = var.root_domain
-  hugo_dir                     = abspath("frontend")
+  hugo_dir                     = abspath("frontend/www")
 }
 
 output "www_s3_bucket_endpoint" {
